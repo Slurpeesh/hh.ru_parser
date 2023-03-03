54731985a7f59bf87594bd4246763dfe11e1aad8
Slurpeesh <JeffNFG@mail.ru>
2023-03-03 19:31:23 +0300

    added progress bar, histogram and logging, fixed bugs and GUI

--- gui.py
+++ gui.py
@@ -1,6 +1,20 @@
+import time
 import tkinter as tk
 from tkinter import ttk
-import time
+import matplotlib
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+matplotlib.use("TkAgg")
+
+
+# function that removes dictionary keys which values are less or equal to "n"
+def remove_deviations(dictionary, n):
+    keys_for_deleting = []
+    for k, v in dictionary.items():
+        if v <= n:
+            keys_for_deleting.append(k)
+    for k in keys_for_deleting:
+        del dictionary[k]
 
 
 # class of Parser app
@@ -13,9 +27,9 @@ class ParserApp:
 
     def __init__(self, command=None):
         # setting size
-        width = 600
-        height = 700
-        self.root.geometry(f'{width}x{height}+600+250')
+        width = 800
+        height = 900
+        self.root.geometry(f'{width}x{height}+600+100')
         self.command = command
         # creating elements in window
         header = tk.Label(self.root, text="hh.ru parser", font=("Arial", 20, "bold"))
@@ -25,8 +39,9 @@ class ParserApp:
         self.query_entry = tk.Entry(self.root, width=25, borderwidth=1, relief="solid", textvariable=self.string_entry)
         accept_button = tk.Button(self.root, text="Search", command=self.command)
         self.waiting = tk.Label(self.root, text="Please, wait, it may take up to a few minutes ...", font=("Arial", 14))
-        self.output = tk.Frame(self.root)
-        self.answer = tk.Text(self.output, height=4, wrap="word", state="disabled", width=30)
+        self.progress_bar = ttk.Progressbar(self.root, orient=tk.HORIZONTAL, length=400)
+        self.output = tk.Frame(self.root, borderwidth=1, relief="solid")
+        self.answer = tk.Text(self.output, height=4, wrap="word", state="disabled", width=45)
         self.result_label = None
 
         # packing elements
@@ -36,21 +51,66 @@ class ParserApp:
         accept_button.grid(row=2, column=0, columnspan=2, pady=5)
         self.waiting.grid(row=3, column=0, columnspan=2, pady=5, sticky="we")
         self.waiting.grid_remove()
+        self.progress_bar.grid(row=4, column=0, columnspan=2, pady=5)
+        self.progress_bar.grid_remove()
         # setting minimum size for each column
-        self.root.grid_columnconfigure(0, minsize=300)
-        self.root.grid_columnconfigure(1, minsize=300)
-        self.output.grid_columnconfigure(0, minsize=577)
+        self.root.grid_columnconfigure(0, minsize=400)
+        self.root.grid_columnconfigure(1, minsize=400)
+        self.output.grid_columnconfigure(0, minsize=777)
         self.output.grid_columnconfigure(1, minsize=23)
 
     # method that shows root window
     def show(self):
         self.root.mainloop()
 
+    # method that creates histogram
+    def create_diagrams(self, data):
+        # creating a clone of data dictionary and remove garbage data from histogram
+        modified_data = data.copy()
+        remove_deviations(modified_data, round(max(data.values())/10))
+        print(modified_data)
+
+        # checking length of data to be appropriately fitted into histogram
+        decreasing_modified_data = {k: v for k, v in sorted(modified_data.items(), key=lambda item: item[1])}
+        for k in decreasing_modified_data:
+            if len(modified_data) <= 15:
+                break
+            del modified_data[k]
+
+        # creating block for plot in tkinter
+        figure = Figure(figsize=(6, 3), dpi=100, layout='constrained')
+        plot = figure.add_subplot(1, 1, 1)
+        canvas = FigureCanvasTkAgg(figure, self.output)
+        canvas.get_tk_widget().grid(row=1, column=0)
+
+        # preparing histogram to user
+        plot.barh(list(modified_data.keys()), list(modified_data.values()), color='darkred')
+        # Remove axes splines
+        for s in ['top', 'bottom', 'left', 'right']:
+            plot.spines[s].set_visible(False)
+
+        # Remove x, y Ticks
+        plot.xaxis.set_ticks_position('none')
+        plot.yaxis.set_ticks_position('none')
+
+        # Add x, y gridlines
+        plot.grid(color='grey', linestyle='-.', linewidth=0.5, alpha=0.3)
+        # Show top values
+        plot.invert_yaxis()
+        # Add Plot Title
+        plot.set_title("Occurrences of key skills in vacancies", loc='left')
+
     # method that shows output text
     def show_results(self, results):
-        self.answer.config(height=int(len(results) / 3 + 2))
+        output_max_height = int(len(results) / 3 + 2)
+        # set restriction to max height of output window
+        if output_max_height > 15:
+            self.answer.config(height=15)
+        else:
+            self.answer.config(height=int(len(results) / 3 + 2))
+
         if not len(self.answer.get("1.0", 'end-1c')):
-            self.answer.grid(row=0, column=0, stick="nswe")
+            self.answer.grid(row=0, column=0, pady=10, stick="nswe")
             y_scroll_output = ttk.Scrollbar(self.output, orient='vertical', command=self.answer.yview)
             self.answer['yscrollcommand'] = y_scroll_output.set
             y_scroll_output.grid(row=0, column=1, stick='ns')
@@ -58,19 +118,38 @@ class ParserApp:
         self.answer.delete("1.0", tk.END)
         self.answer.insert("1.0", results)
         self.answer.config(state="disabled")
+        self.create_diagrams(results)
         self.output.grid(row=4, column=0, columnspan=2, sticky="wens")
 
     # method that shows waiting message
     def create_waiting(self):
         self.waiting.grid()
+        self.progress_bar.grid()
         self.root.update()
 
     # method that deletes waiting message
     def destroy_waiting(self):
         self.waiting.destroy()
+        self.progress_bar.destroy()
+        # recreate waiting label
         self.waiting = tk.Label(self.root, text="Please, wait, it may take up to a few minutes ...", font=("Arial", 14))
         self.waiting.grid(row=3, column=0, columnspan=2, pady=5, sticky="we")
         self.waiting.grid_remove()
+        # recreate progress bar
+        self.progress_bar = ttk.Progressbar(self.root, orient=tk.HORIZONTAL)
+        self.progress_bar.grid(row=4, column=0, columnspan=2, pady=5)
+        self.progress_bar.grid_remove()
+
+    # method that sets maximum value of progress bar (times 10 for smooth animation)
+    def set_max_value_progress_bar(self, value):
+        self.progress_bar.config(maximum=value*30)
+
+    # method that updates progress bar
+    def set_progress_bar(self, value):
+        for step in range((value-1)*30, value*30+1):
+            self.progress_bar.config(value=step)
+            self.root.update()
+            time.sleep(0.01)
 
     # method that deletes output text
     def clear_output(self):
@@ -79,7 +158,7 @@ class ParserApp:
             self.output = tk.Frame(self.root)
             self.answer = tk.Text(self.output, height=4, wrap="word", state="disabled", width=30)
             self.result_label = None
-            self.output.grid_columnconfigure(0, minsize=577)
+            self.output.grid_columnconfigure(0, minsize=777)
             self.output.grid_columnconfigure(1, minsize=23)
 
 
@@ -88,36 +167,115 @@ def test():
     app.create_waiting()
     time.sleep(2)
     app.destroy_waiting()
-    text = {5: "dsggggggggggggggggggggggggggggggggggggggg",
-            6: "dsgsdgdsgdsggggggdgdgsdssgddddddddddddddd",
-            7: "sgddddddddddddddddddddddddddddddddddddddd",
-            8: "dsggggggggggggggggggggggggggggggggggggggg",
-            9: "dsgsdgdsgdsggggggdgdgsdssgddddddddddddddd",
-            10: "sgddddddddddddddddddddddddddddddddddddddd",
-            11: "dsggggggggggggggggggggggggggggggggggggggg",
-            12: "dsgsdgdsgdsggggggdgdgsdssgddddddddddddddd",
-            13: "sgddddddddddddddddddddddddddddddddddddddd",
-            14: "dsggggggggggggggggggggggggggggggggggggggg",
-            15: "dsgsdgdsgdsggggggdgdgsdssgddddddddddddddd",
-            16: "sgddddddddddddddddddddddddddddddddddddddd",
-            17: "dsggggggggggggggggggggggggggggggggggggggg",
-            18: "dsgsdgdsgdsggggggdgdgsdssgddddddddddddddd",
-            19: "sgddddddddddddddddddddddddddddddddddddddd",
-            20: "dsggggggggggggggggggggggggggggggggggggggg",
-            21: "dsgsdgdsgdsggggggdgdgsdssgddddddddddddddd",
-            22: "sgddddddddddddddddddddddddddddddddddddddd",
-            23: "dsggggggggggggggggggggggggggggggggggggggg",
-            24: "dsgsdgdsgdsggggggdgdgsdssgddddddddddddddd",
-            25: "sgddddddddddddddddddddddddddddddddddddddd",
-            26: "dsggggggggggggggggggggggggggggggggggggggg",
-            27: "dsgsdgdsgdsggggggdgdgsdssgddddddddddddddd",
-            28: "sgddddddddddddddddddddddddddddddddddddddd",
-            29: 1, 30: 30, 31: 31, 32: 32, 33: 33, 34: 34, 35: 35,
-            36: 36, 37: 37, 38: 38, 39: 39, 40: 40, 41: 41, 42: 42,
-            43: 43, 44: 44, 45: 45, 46: 46, 47: 47, 48: 48, 49: 49,
-            50: 50, 51: 51, 52: 52, 53: 53, 54: 54, 55: 55, 56: 56,
-            57: 57, 58: 58, 59: 59, 60: 60, 61: 61, 62: 62, 63: 63,
-            64: 64, 65: 65, 66: 66, 67: 67, 68: 68, 69: 69, 70: 70}
+    text = {'Python': 164,
+            'Linux': 80,
+            'SQL': 77,
+            'Git': 61,
+            'Docker': 45,
+            'PostgreSQL': 34,
+            'Bash': 29,
+            'C++': 24,
+            'CI/CD': 18,
+            'Java': 17,
+            'RabbitMQ': 15,
+            'MySQL': 15,
+            'Ansible': 15,
+            'Django Framework': 14,
+            'Английский\xa0— B1 — Средний': 14,
+            'Jenkins': 14,
+            'Kubernetes': 14,
+            'TCP/IP': 14,
+            'Английский язык': 13,
+            'ООП': 12,
+            'Базы данных': 11,
+            'Тестирование': 11,
+            'Flask': 9,
+            'Nginx': 9,
+            'СУБД': 9,
+            'Hadoop': 9,
+            'Английский\xa0— B2 — Средне-продвинутый': 9,
+            'QA': 9,
+            'ClickHouse': 8,
+            'Redis': 8,
+            'Анализ данных': 8,
+            'Spark': 8,
+            'ETL': 8,
+            'MongoDB': 7,
+            'Django': 7,
+            'Информационные технологии': 7,
+            'Управление проектами': 7,
+            'PyTorch': 7,
+            'SCALA': 7,
+            'Gitlab': 7,
+            'MS SQL': 7,
+            'Qt': 7,
+            'ELK': 7,
+            'Elasticsearch': 6,
+            'JavaScript': 6,
+            'Английский\xa0— A1 — Начальный': 6,
+            'REST API': 6,
+            'DWH': 6,
+            'C#': 6,
+            'Kafka': 6,
+            'ML': 6,
+            'C/C++': 6,
+            'Администрирование серверов Linux': 6,
+            'Математическое моделирование': 6,
+            'Английский\xa0— A2 — Элементарный': 5,
+            'Tableau': 5,
+            'Pandas': 5,
+            'Airflow': 5,
+            'Функциональное тестирование': 5,
+            'Golang': 5,
+            'Grafana': 5,
+            'Информационная безопасность': 5,
+            'Администрирование': 5,
+            'SQLAlchemy': 4,
+            'Работа в команде': 4,
+            'Docker-compose': 4,
+            'API': 4,
+            'Machine Learning': 4,
+            'Computer Vision': 4,
+            'DevOps': 4,
+            'MS Excel': 4,
+            'Gitlab CI': 4,
+            'Автоматизация тестирования': 4,
+            'Go': 4,
+            'Clickhouse': 4,
+            'A/B тесты': 4,
+            'Atlassian Jira': 4,
+            'Groovy': 4,
+            'Prometheus': 4,
+            'Регрессионное тестирование': 4,
+            'aiohttp': 3,
+            'R': 3,
+            'asyncio': 3,
+            'Машинное обучение': 3,
+            'HTTP': 3,
+            'Руководство коллективом': 3,
+            'Асинхронное программирование': 3,
+            'PyCharm': 3,
+            'Data Science': 3,
+            'Разработка ПО': 3,
+            'Имитационное моделирование': 3,
+            'Ручное тестирование': 3,
+            'XML': 3,
+            'K8S': 3,
+            'Аналитическое мышление': 3,
+            'NLP': 3,
+            'HTML': 3,
+            'CSS': 3,
+            'SVN': 3,
+            'Нейронные сети': 3,
+            'Ruby': 3,
+            'CentOS': 3,
+            'Zabbix': 3,
+            'Cisco': 3,
+            'OpenShift': 3,
+            'Деловая переписка': 3,
+            'REST': 3, 'Английский\xa0— C1 — Продвинутый': 3,
+            'Power BI': 3,
+            'Технические средства информационной защиты': 3}
     app.show_results(text)
 
 
--- main.py
+++ main.py
@@ -2,19 +2,10 @@ import requests
 import time
 import os
 import math
+from loguru import logger
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 import gui
-
-
-# function that removes dictionary keys which values are less or equal to "n"
-def remove_deviations(dictionary, n):
-    keys_for_deleting = []
-    for k, v in dictionary.items():
-        if v <= n:
-            keys_for_deleting.append(k)
-    for k in keys_for_deleting:
-        del dictionary[k]
 
 
 # function that returns a fraction of specified number "n"
@@ -31,6 +22,10 @@ def summary_push(elem):
         summary_dict[elem] = 1
 
 
+# clear log if exist
+if os.path.isfile("log.log"):
+    os.remove("log.log")
+
 # initial variables
 summary_dict = {}
 user_agent = UserAgent()
@@ -39,8 +34,12 @@ headers = {
     "User-Agent": user_agent.random
 }
 
+# preparing logging
+logger.add("log.log", format="{time}   | {level} |   {message}", level="DEBUG")
+
 
 # main function of parsing algorithm
+@logger.catch
 def get_statistics():
     global summary_dict
     # deleting output frame if exists
@@ -49,6 +48,7 @@ def get_statistics():
     gui_app.create_waiting()
     # getting response
     search_request = gui_app.string_entry.get()
+    logger.info(f'Search request: {search_request}')
     url = f'https://hh.ru/search/vacancy?text={search_request}&area=2'
     response = requests.get(url, headers=headers)
     src = response.text
@@ -63,36 +63,50 @@ def get_statistics():
 
     # getting a number of amount of all pages
     try:
-        last_page = soup.find(class_=["bloko-gap", "bloko-gap_top"]).findAll(class_="pager-item-not-in-short-range")[-1]
+        pager = soup.find(class_="pager")
+        last_page = pager.findChildren('span', recursive=False)[-1]
         last_page_num = int(last_page.find('a').text)
-    except IndexError:
+    except AttributeError:
         last_page_num = 1
 
     # last pages are usually irrelevant, so only 2/3 of all pages will be considered
     num_of_pages = get_fraction(last_page_num, 2 / 3)
+    logger.info(f'Total num of pages: {last_page_num}')
+    logger.info(f'Pages to be considered: {num_of_pages}')
+
+    # considering progress bar, 20 vacancies on each page
+    vacancies = None
+    if last_page_num == 1:
+        vacancies = soup.findAll('a', class_="serp-item__title")
+        num_of_vacancies = len(vacancies)
+        gui_app.set_max_value_progress_bar(num_of_vacancies)
+    else:
+        gui_app.set_max_value_progress_bar(20 * num_of_pages)
+    progress_step = 0
 
     # going through all pages
     for cur_page in range(num_of_pages):
         url = f'https://hh.ru/search/vacancy?text={search_request}&area=2&page={cur_page}'
-        response = requests.get(url, headers=headers)
-        print("Currently on page", cur_page + 1, response.url)
-        src = response.text
-        with open("page.html", 'w') as file:
-            file.write(src)
-        with open("page.html") as file:
-            src = file.read()
+        if vacancies is None:
+            response = requests.get(url, headers=headers)
+            src = response.text
+            with open("page.html", 'w') as file:
+                file.write(src)
+            with open("page.html") as file:
+                src = file.read()
+            soup = BeautifulSoup(src, "lxml")
+            vacancies = soup.findAll('a', class_="serp-item__title")
 
-        soup = BeautifulSoup(src, "lxml")
-        vacancies = soup.findAll('a', class_="serp-item__title")
+        logger.info(f'Currently on page {cur_page + 1}: {response.url}')
         vacancies_url_list = []
         for vacancy in vacancies:
             vacancies_url_list.append(vacancy['href'])
 
-        time.sleep(1.5)
+        time.sleep(1)
         # going through all vacancies on current page
         for vacancy_url in vacancies_url_list:
             response = requests.get(vacancy_url, headers=headers)
-            print("Current vacancy:", response.url)
+            logger.info(f'Current vacancy: {response.url}')
             src = response.text
 
             # saving html into file
@@ -105,19 +119,24 @@ def get_statistics():
             keywords = soup.findAll(class_=["bloko-tag__section", "bloko-tag__section_text"])
             for keyword in keywords:
                 summary_push(keyword.text)
-            time.sleep(1.5)
+            # updating progress bar
+            progress_step += 1
+            gui_app.set_progress_bar(progress_step)
+            time.sleep(1)
 
     # deleting html files
     if os.path.isfile("page.html") and os.path.isfile("vacancy.html"):
         os.remove("page.html")
         os.remove("vacancy.html")
     # preparing data to user
-    remove_deviations(summary_dict, 2)
+    gui.remove_deviations(summary_dict, 2)
     summary_dict = dict(sorted(summary_dict.items(), key=lambda item: item[1], reverse=True))
     # deleting waiting message
     gui_app.destroy_waiting()
     # showing output frame with summary_dict info
     gui_app.show_results(summary_dict)
+    # adding summary_dict info into log
+    logger.info(f'Result: {summary_dict}')
     # summary_dict should be reset
     summary_dict = {}
 
